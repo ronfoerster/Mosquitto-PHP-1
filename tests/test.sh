63ff313c6843837cc4ab80dbc80142ca1e99c082
ronfoerster <101582425+ronfoerster@users.noreply.github.com>
2022-03-21 18:20:28 +0100

    Update test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -2,5 +2,7 @@
 
 echo -e "extension_dir=/usr/lib/php/20210902" >> /etc/php/8.1/cli/conf.d/15-mosquitto-php.ini
 echo -e "extension=mosquitto.so" >> /etc/php/8.1/cli/conf.d/15-mosquitto-php.ini
-php --ini
-cat /etc/php/8.1/cli/conf.d/15-mosquitto-php.ini
+#php --ini
+#cat /etc/php/8.1/cli/conf.d/15-mosquitto-php.ini
+echo -e "extension_dir=/usr/lib/php/20210902" >> /home/runner/work/Mosquitto-PHP-1/Mosquitto-PHP-1/tmp-php.ini
+echo -e "extension=mosquitto.so" >> /home/runner/work/Mosquitto-PHP-1/Mosquitto-PHP-1/tmp-php.ini
