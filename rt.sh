3f121b9a99e87bcec2050cd04b8a87c3778054eb
ronfoerster <>
2022-03-24 14:31:33 +0100

    new

--- rt.sh
+++ rt.sh
@@ -5,6 +5,9 @@ cd tests
 sudo systemctl stop mosquitto
 ./makeTestCerts.sh
 sudo chown -R mosquitto:mosquitto certs
+echo "$GITHUB_ACTION_PATH"
+id
+ls -al certs
 mosquitto -c mosquitto.conf
 cd ..
 #make test
